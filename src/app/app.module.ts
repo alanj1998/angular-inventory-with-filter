b198b96b733e39fe8af38dfa3e93c79f6f7ca4b0
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-09-30 23:09:42 +0100

    finished without star rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GetDataService } from '../services/get-data.service';
+import { RemoveHyphenPipe } from './remove-hyphen.pipe';
+import { StarratingComponent } from './starrating/starrating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    RemoveHyphenPipe,
+    StarratingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/starrating/starrating.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-starrating',
+  templateUrl: './starrating.component.html',
+  styleUrls: ['./starrating.component.css']
+})
+export class StarratingComponent implements OnInit {
+
+  @Input() rating : number
+  starWidth: number
+  constructor() { }
+
+  ngOnInit() {
+    this.starWidth = this.rating * 86 / 5;
+    console.log(this.rating)
+  }
+
+}
